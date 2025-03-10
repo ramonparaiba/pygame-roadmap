ea0a312bbd0c7dba4b53b78cb27b5f59223d0326
ramonparaiba <ramonparaiba@gmail.com>
2025-03-10 18:26:02 -0300

    feat: desenhou triangulo usando a função draw.poygon

--- 002-Desenharformasbásicas/formasBasicas.py
+++ 002-Desenharformasbásicas/formasBasicas.py
@@ -7,17 +7,23 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Formas básicas")
 
 running = True
+screen.fill((50, 0, 0))
+triangulo_coordenadas = [
+    (550, 30), (500, 100), (600, 100)
+]
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     #Atualizar o fundo da tela
-    screen.fill((0, 0, 0))
+    
 
     #Desenhos
     pygame.draw.rect(screen, (0, 255, 0), (100, 100, 60, 60)) # retangulo verde de 60x60 na posição (100, 100)
-    pygame.draw.circle(screen, (255, 0, 0), (300, 300), 60) #círculo vermelho
+    pygame.draw.circle(screen, (255, 0, 0), (300, 300), 60) #círculo vermelho de raio 60
+
+    pygame.draw.polygon(screen, (0, 0, 100), triangulo_coordenadas)
 
     # Atualiza a tela para exibir os desenhos
     pygame.display.flip()
